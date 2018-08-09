39d8c94e8213911fd8ed55816c14ad3faa31687a
disco_nap <jrnapolit@gmail.com>
2018-08-08 18:46:53 -0700

    PyBank removed instructions and cleaned up some unused lines of code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,7 @@
-#Your task is to create a Python script that analyzes the records to calculate each of the following:
 import os
 import csv
 
 csvpath = os.path.join("..", "..", "budget_data.csv")
-
-
-month = []
-profit_losses = []
 
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
@@ -16,7 +11,6 @@ with open(csvpath, newline="") as csvfile:
     total = 0
     maximum = 0
     minimum = 0
-
 
     for row in csvreader:
         count = count + 1
@@ -29,7 +23,6 @@ with open(csvpath, newline="") as csvfile:
             minimum = int(row[1])
             mindate = row[0]
         
-
 print("Financial Analysis")
 print("------------------------")
 print(f'Total months: {count}')
@@ -39,26 +32,4 @@ print(f'Greatest Increase in Profits: {maxdate} ${maximum:.2f}')
 print(f'Greatest Decrease in Profits: {mindate} ${minimum:.2f}')
 
 
-
-
-
-
-
-#The total number of months included in the dataset
-
-#The total net amount of "Profit/Losses" over the entire period
-
-#The average change in "Profit/Losses" between months over the entire period
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
-
-#As an example, your analysis should look similar to the one below:
-
-#  Financial Analysis
-#  ----------------------------
-#  Total Months: 86
-#  Total: $38382578
-#  Average  Change: $-2315.12
-#  Greatest Increase in Profits: Feb-2012 ($1926159)
-#  Greatest Decrease in Profits: Sep-2013 ($-2196167)
 # In addition, your final script should both print the analysis to the terminal and export a text file with the results.
